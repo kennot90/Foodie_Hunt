7da2bdf4a0a5698efa2653eeaece9012a484c06c
Ken <gohkenrick@gmail.com>
2019-03-21 10:00:15 +0800

    Updated with comments

--- Python_Scripts/CA_NLP_Sentimental_Analysis.py
+++ Python_Scripts/CA_NLP_Sentimental_Analysis.py
@@ -1,14 +1,23 @@
+##This code is a simple implementation of the sentiment analysis using Vader
+
+
+
 from textblob import TextBlob
 from nltk.sentiment.vader import SentimentIntensityAnalyzer
 
+
+#initialize the MongoDB
 from pymongo import MongoClient
 client = MongoClient()
 db = client['charles_ca']
+
+
 restaurants_collection = db.restaurants
 events_collection = db.events
 customers_collection = db.customers
 reviews = db.reviews
 
+#Obtain the Sentiment Analysis
 sid = SentimentIntensityAnalyzer()
 for post in reviews.find().limit(4):
     text = (post['Text'])
